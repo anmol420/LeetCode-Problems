3d135592ac33e9ca2a8361af81af57f40ed9a1bb
anmol420 <anmolanand2k15@gmail.com>
2023-08-27 22:18:06 +0530

    Added Easy Questions

---
+++ Easy/palindrome.java
@@ -0,0 +1,31 @@
+class palindrome {
+    public static int Digits(int x) {
+        int c=0,d=0;
+        while (x!=0) {
+            d=x%10;
+            c++;
+            x/=10;
+        }
+        return c;
+    }
+    public boolean isPalindrome(int x) {
+        int y=Digits(x);
+        y--;
+        int x1=x;
+        int sum=0,n1=0,d=0;
+        if (x1<0) {
+            return false;
+        }
+        while (y>=0) {
+            d=x1/(int)(Math.pow(10,y));
+            x1=x1%(int)(Math.pow(10,y));
+            y--;
+            sum=sum+(int)(d*Math.pow(10,n1));
+            n1++;
+        }
+        if (sum==x) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Easy/two_sum.java
@@ -0,0 +1,25 @@
+class two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        int x=0;
+        int b[]=new int[2];
+        for (int i=0;i<n;i++) {
+            x++;
+            if (x==n) {
+                break;
+            }
+            if (nums[i]+nums[x] == target) {
+                b[0]=i;
+                b[1]=x;
+            } else {
+                for (int j=0;j<n;j++) {
+                     if (nums[i]+nums[j] == target && i!=j) {
+                        b[0]=i;
+                        b[1]=j;
+                    }
+                }
+            }
+        }
+        return b;
+    }
+}
