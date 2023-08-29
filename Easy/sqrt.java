6321ad5d605c2f3d80afa6be1edd3162c7556f3c
anmol420 <anmolanand2k15@gmail.com>
2023-08-29 11:16:09 +0530

    Added Easy Question

---
+++ Easy/sqrt.java
@@ -0,0 +1,9 @@
+public class sqrt {
+    public static int mySqrt(int x) {
+        int s=(int)Math.sqrt(x);
+        return s;
+    }
+    public static void main(String[] args) {
+        System.out.println(mySqrt(0));
+    }
+}
